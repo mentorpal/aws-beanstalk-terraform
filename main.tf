73e5efbbe9def62ab592395e8a079dc3b0f5d3ee
larry kirschner <larrykirschner@gmail.com>
2021-04-30 17:47:17 -0700

    release transcribe

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ locals {
 }
 
 module "transcribe_aws" {
-    source                  = "git::https://github.com/ICTLearningSciences/py-transcribe-aws.git?ref=tags/1.4.0-alpha.1"
+    source                  = "git::https://github.com/ICTLearningSciences/py-transcribe-aws.git?ref=tags/1.4.0"
     transcribe_namespace    = local.namespace
 }
 
