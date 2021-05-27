b2afcd2304e808b7b20b777192ed1f39dd62b885
larry kirschner <larrykirschner@gmail.com>
2021-05-27 15:07:10 -0700

    STATIC_UPLOAD => STATIC

--- main.tf
+++ main.tf
@@ -165,10 +165,10 @@ module "elastic_beanstalk_environment" {
                             TRANSCRIBE_AWS_REGION=var.aws_region,
                             TRANSCRIBE_AWS_SECRET_ACCESS_KEY=module.transcribe_aws.transcribe_env_vars.TRANSCRIBE_AWS_SECRET_ACCESS_KEY,
                             TRANSCRIBE_AWS_S3_BUCKET_SOURCE=module.transcribe_aws.transcribe_env_vars.TRANSCRIBE_AWS_S3_BUCKET_SOURCE,
-                            STATIC_UPLOAD_AWS_ACCESS_KEY_ID        = aws_iam_access_key.static_upload_policy_access_key.id,
-                            STATIC_UPLOAD_AWS_SECRET_ACCESS_KEY    = aws_iam_access_key.static_upload_policy_access_key.secret,
-                            STATIC_UPLOAD_AWS_REGION               = var.aws_region,
-                            STATIC_UPLOAD_AWS_S3_BUCKET            = module.cdn_static.s3_bucket
+                            STATIC_AWS_ACCESS_KEY_ID        = aws_iam_access_key.static_upload_policy_access_key.id,
+                            STATIC_AWS_SECRET_ACCESS_KEY    = aws_iam_access_key.static_upload_policy_access_key.secret,
+                            STATIC_AWS_REGION               = var.aws_region,
+                            STATIC_AWS_S3_BUCKET            = module.cdn_static.s3_bucket
                             STATIC_URL_BASE                        = "https://${local.static_alias}"
                           }
                         )
