6be5fdc117c9ccc6341b5e6cc45fd723db193a45
Milan Simonovic <mbsimonovic@gmail.com>
2022-05-13 23:29:53 +0200

    fix script path

--- main.tf
+++ main.tf
@@ -329,7 +329,7 @@ resource "aws_cloudfront_function" "cf_fn_origin_root" {
   runtime = "cloudfront-js-1.0"
   comment = "Rewrites root s3 bucket requests to index.html for all apps (home, chat, admin)"
   publish = true
-  code    = file("scripts/mentorpal-rewrite-default-index-s3-origin.js")
+  code    = file("${path.module}/scripts/mentorpal-rewrite-default-index-s3-origin.js")
 }
 
 module "cdn_beanstalk" {
