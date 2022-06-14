c4f5ab40cddd514e3df266dcdb0f2dc72edcb7fe
Milan Simonovic <mbsimonovic@gmail.com>
2022-06-14 13:14:49 +0200

    fix typo

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "s3_static_bucket_arn" {
   description = "s3 bucket that holds static assets (e.g. videos, thumbnails, ...)"
-  value       = module.cdn_content.s3_bucket_arn
+  value       = module.cdn_static.s3_bucket_arn
 }
