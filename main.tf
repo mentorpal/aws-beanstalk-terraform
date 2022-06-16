3d01ca33fa20f95e2e4c2045e9d428acbfd8c9c1
Milan Simonovic <mbsimonovic@gmail.com>
2022-06-16 13:16:57 +0200

    enable content backup

--- main.tf
+++ main.tf
@@ -100,6 +100,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "content_bucket_version_expire_
   }
 }
 
+module "content_backup" {
+  count  = var.enable_content_backup ? 1 : 0
+  source = "./modules/backup"
+  name   = "${var.eb_env_name}-s3-content-backup-${var.eb_env_stage}"
+  resources = [
+    module.cdn_static.s3_bucket_arn
+  ]
+  tags = var.eb_env_tags
+}
+
 #####
 # Firewall
 # 
